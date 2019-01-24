0f87d9fca365a75254806210eae4a60eaa2e7e9e
Ken Duigraha Putra <ken.putra@bizzy.co.id>
2019-01-24 18:16:52 +0700

    Set Uikit to project nuxt.js.

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,12 +24,12 @@ module.exports = {
   /*
   ** Global CSS
   */
-  css: [],
+  css: ['uikit/dist/css/uikit.css'],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [],
+  plugins: [{ src: '~/plugins/uikit.js', ssr: false }],
 
   /*
   ** Nuxt.js modules
